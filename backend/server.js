33130ffe1c320e6c0cf5893a5852dddb25f75c74
Aled <aledhole2@hotmail.com>
2025-02-10 15:24:01 +1000

    live-prices

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cryptoRoutes = require("./router");
+const app = express();
+const PORT = 5000;
+app.use(cors({credentials: true }))
+app.use("/", cryptoRoutes);
+
+
+require("dotenv").config();
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Proxy server running on http://localhost:${PORT}`);
+    console.log(`Using API Key: ${process.env.CMC_SECRET_KEY ? "Loaded" : "NOT LOADED"}`);
+});
